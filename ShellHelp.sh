09f33f4904d5b70aa0ac97af16e35a458cf9312f
Sanat Nayak <95233999+SanatNayak@users.noreply.github.com>
2022-04-28 20:07:07 +0530

    Add files via upload

---
+++ ShellHelp.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+clear
+printf "\e[1;32m:'######::'##::::'##:'########:'##:::::::'##::::::::::'##::::'##:'########:'##:::::::'########::\e[0m\n"
+printf "\e[1;32m'##... ##: ##:::: ##: ##.....:: ##::::::: ##:::::::::: ##:::: ##: ##.....:: ##::::::: ##.... ##:\e[0m\n"
+printf "\e[1;32m ##:::..:: ##:::: ##: ##::::::: ##::::::: ##:::::::::: ##:::: ##: ##::::::: ##::::::: ##:::: ##:\e[0m\n"
+printf "\e[1;32m. ######:: #########: ######::: ##::::::: ##:::::::::: #########: ######::: ##::::::: ########::\e[0m\n"
+printf "\e[1;32m:..... ##: ##.... ##: ##...:::: ##::::::: ##:::::::::: ##.... ##: ##...:::: ##::::::: ##.....:::\e[0m\n"
+printf "\e[1;32m'##::: ##: ##:::: ##: ##::::::: ##::::::: ##:::::::::: ##:::: ##: ##::::::: ##::::::: ##::::::::\e[0m\n"
+printf "\e[1;32m. ######:: ##:::: ##: ########: ########: ########:::: ##:::: ##: ########: ########: ##::::::::\e[0m\n"
+printf "\e[1;32m:......:::..:::::..::........::........::........:::::..:::::..::........::........::..:::::::::\e[0m\n"
+printf "         \e[1;96m                                                                         Version 2022.4"
+printf "         \e[1;36m                                 GreyHackers                      "
+
+
+printf "\n"                              
+printf "\e[1;34m  #####                                #     #                            \e[0m\n"
+printf "\e[1;34m #     #   ##   #    #   ##   #####    ##    #   ##   #   #   ##   #    # \e[0m\n"
+printf "\e[1;34m #        #  #  ##   #  #  #    #      # #   #  #  #   # #   #  #  #   #  \e[0m\n"
+printf "\e[1;34m  #####  #    # # #  # #    #   #      #  #  # #    #   #   #    # ####   \e[0m\n"
+printf "\e[1;34m       # ###### #  # # ######   #      #   # # ######   #   ###### #  #   \e[0m\n"
+printf "\e[1;34m #     # #    # #   ## #    #   #      #    ## #    #   #   #    # #   #  \e[0m\n"
+printf "\e[1;34m  #####  #    # #    # #    #   #      #     # #    #   #   #    # #    # \e[0m\n"
+
+printf "\n"
+printf "\n"
+nam=$(whoami)
+echo "Your User Name: $nam"
+use=$(pwd)
+echo "Your Current Directory Is: $use"
+printf "\n"
+echo ""
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
+echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  !!    Update And Upgrade Your Install    !!\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------- \e[1;91m\e[0m'
+echo ""
+apt update && apt upgrade -y
+echo""
+clear
+printf "\n"                              
+printf "\e[1;34m  #####                                #     #                            \e[0m\n"
+printf "\e[1;34m #     #   ##   #    #   ##   #####    ##    #   ##   #   #   ##   #    # \e[0m\n"
+printf "\e[1;34m #        #  #  ##   #  #  #    #      # #   #  #  #   # #   #  #  #   #  \e[0m\n"
+printf "\e[1;34m  #####  #    # # #  # #    #   #      #  #  # #    #   #   #    # ####   \e[0m\n"
+printf "\e[1;34m       # ###### #  # # ######   #      #   # # ######   #   ###### #  #   \e[0m\n"
+printf "\e[1;34m #     # #    # #   ## #    #   #      #    ## #    #   #   #    # #   #  \e[0m\n"
+printf "\e[1;34m  #####  #    # #    # #    #   #      #     # #    #   #   #    # #    # \e[0m\n"
+echo ""
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
+echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  !! All Type Requirements Package Install !!\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------- \e[1;91m\e[0m'
+echo ""
+apt-get install apache2 -y
+apt install php -y
+pkg install python
+apt install tail -y
+apt install curl -y
+apt install unrar -y
+systemctl start apache2
+apt install ruby -y
+apt-get install ruby -y
+apt install gem -y
+gem install lolcat
+apt install gnome-terminal -y
+sudo apt install wget -y
+git clone https://github.com/SanatNayak/HackCam.git
+git clone https://github.com/SanatNayak/YT-Downloader.git
+echo""
+clear
+printf "\n"                              
+printf "\e[1;34m  #####                                #     #                            \e[0m\n"
+printf "\e[1;34m #     #   ##   #    #   ##   #####    ##    #   ##   #   #   ##   #    # \e[0m\n"
+printf "\e[1;34m #        #  #  ##   #  #  #    #      # #   #  #  #   # #   #  #  #   #  \e[0m\n"
+printf "\e[1;34m  #####  #    # # #  # #    #   #      #  #  # #    #   #   #    # ####   \e[0m\n"
+printf "\e[1;34m       # ###### #  # # ######   #      #   # # ######   #   ###### #  #   \e[0m\n"
+printf "\e[1;34m #     # #    # #   ## #    #   #      #    ## #    #   #   #    # #   #  \e[0m\n"
+printf "\e[1;34m  #####  #    # #    # #    #   #      #     # #    #   #   #    # #    # \e[0m\n"
+echo ""
+printf "         \e[1;36m                                 GreyHackers                      "
+echo ""
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------  \e[1;91m\e[0m'
+echo -e $'\e[1;96m\e[0m\e[1;77m\e[0m\e[1;96m\e[0m\e[1;91m  !!         DOWNLOAD Link Services        !!\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;91m\e[0m\e[1;96m\e[0m\e[1;91m   ----------------------------------------- \e[1;91m\e[0m'
+echo ""
+wget -q --show-progress https://github.com/Online-Hacking/Mr-OnlineHacking/raw/main/Ngrok/Ngrok%20Old/ngrok -O ngrok
+wget -q --show-progress https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-386 -O cloudflared-linux-386
+echo""
+clear 
+
+sleep 2
+echo
+echo
+clear
+printf "         \e[1;36m                                 GreyHackers                      "
+
+echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;96m\e[0m\e[1;92m  ---------------------------------   \e[1;91m\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;90m\e[0m\e[1;92m !!    Installation Successfull   !!  \e[1;91m\e[0m'
+echo -e $'\e[1;91m\e[0m\e[1;33m\e[0m\e[1;96m\e[0m\e[1;92m  ---------------------------------   \e[1;91m\e[0m'
+echo "\e[1;36m   This Tool Creat By Sanat\e[0m\n"
+sleep 4
+echo
+echo -e "\e[1m \e[36m[+] Ready Your Termux \e[m \e[21"
+echo
+fi
+sleep 3
+echo
+echo "\e[1;36m [+]Our Special Mambers\e[m \e[21"
+echo "\e[1;36m    Sayed Saad\e[0m\n"
+echo "\e[1;36m    Sarthak\e[0m\n"
+echo "\e[1;36m    Zain\e[0m\n"
+rm -rf ShellHelp.sh
+exit
